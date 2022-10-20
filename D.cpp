a168eb9eb9b3bdbaa128867d57ed6611a0b04e40
sergeyr0man0v <romanovs1337@gmail.com>
2022-10-20 23:09:31 +0300

    D update

--- D.cpp
+++ D.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <iostream>
 
-void siftDown(long long* a, int v, int size) {
+void siftDown(unsigned int* a, int v, int size) {
   while (2 * v + 1 < size) {
     int u = 2 * v + 1;
     if (u + 1 < size && a[u] > a[u + 1]) {
@@ -15,19 +15,19 @@ void siftDown(long long* a, int v, int size) {
   }
 }
 
-void reverse(long long* a, int size) {
+void reverse(unsigned int* a, int size) {
   for (int i = 0; i < size / 2; ++i) {
     std::swap(a[i], a[size - i - 1]);
   }
 }
 
-void heapify(long long* a, int size) {
+void heapify(unsigned int* a, int size) {
   for (int i = size - 1; i >= 0; --i) {
     siftDown(a, i, size);
   }
 }
 
-int extractMin(long long* a, int size) {
+int extractMin(unsigned int* a, int size) {
   size--;
   int min_val = a[0];
   std::swap(a[0], a[size--]);
@@ -35,7 +35,7 @@ int extractMin(long long* a, int size) {
   return min_val;
 }
 
-void MyHeapSort(long long* a, int size) {
+void MyHeapSort(unsigned int* a, int size) {
   int cur = size;
   heapify(a, size);
   for (int i = 0; i < size; ++i) {
@@ -44,8 +44,8 @@ void MyHeapSort(long long* a, int size) {
   reverse(a, size);
 }
 
-long long* Merge(long long* a, long long* b, int k, int size_a, int size_b) {
-  long long* d = new long long[k];
+unsigned int* Merge(unsigned int* a, unsigned int* b, int k, int size_a, int size_b) {
+  unsigned int* d = new unsigned int[k];
   int i = 0, j = 0;
   for (int id = 0; id < k; ++id) {
     if (i < size_a && j < size_b) {
@@ -69,9 +69,9 @@ long long* Merge(long long* a, long long* b, int k, int size_a, int size_b) {
 
 int main() {
   int n, k;
-  long long a0, x, y;
+  unsigned int a0, x, y;
   std::cin >> n >> k >> a0 >> x >> y;
-  long long a[10000];
+  unsigned int a[10000];
   a[0] = (x * a0 + y) % static_cast<long long>(std::pow(2, 30));;
   for (int i = 1; i < n; ++i) {
     a[i] = (x * a[i - 1] + y) % static_cast<long long>(std::pow(2, 30));
@@ -79,11 +79,13 @@ int main() {
   for (int i = 0; i <= (n - 1) / k; ++i) {
     MyHeapSort(a + i * k, std::min(k, n - i * k));
   }
-  for (int i = 1; i <= (n - 1) / k; ++i) {
-    Merge(a, a + i * k, k, k, std::min(k, n - i * k));
+  unsigned int* c = Merge(a, a + k, k, k, std::min(k, n - k));
+  for (int i = 2; i <= (n - 1) / k; ++i) {
+    c = Merge(c, a + i * k, k, k, std::min(k, n - i * k));
   }
   for (int i = 0; i < k; ++i) {
-    std::cout << a[i] << ' ';
+    std::cout << c[i] << ' ';
   }
+  delete[] c;
   return 0;
 }
--- main.cpp
+++ main.cpp
@@ -2,46 +2,47 @@
 #include <iostream>
 
 struct Heap {
-    int a[100000] = {};
-    int size;
-     Heap() { size = 0; }
+  int a[100000] = {};
+  int size;
 
-     void Insert(int x) {
-       a[size++] = x;
-       SiftUp(size);
-     }
+  Heap() { size = 0; }
 
-     void SiftUp(int n) {
-       while ((n - 1) / 2 != 0 && a[(n - 1) / 2] > a[n]) {
-         std::swap(a[n], a[(n - 1) / 2]);
-         n = (n - 1) / 2;
-       }
-     }
+  void Insert(int x) {
+    a[size++] = x;
+    SiftUp(size);
+  }
 
-     void SiftDown(int n) {
-       while (2 * n + 1 <= size) {
-         if (2 * n + 2 <= n && a[2 * n + 2] < a[2 * n + 1] && a[n] > a[2 * n + 2]) {
-           std::swap(a[n], a[2 * n + 2]);
-           n = 2 * n + 2;
-         } else if (a[n] > a[2 * n + 1]) {
-           std::swap(a[n], a[2 * n + 1]);
-           n = 2 * n + 1;
-         } else {
-           break;
-         }
-       }
-     }
+  void SiftUp(int n) {
+    while ((n - 1) / 2 != 0 && a[(n - 1) / 2] > a[n]) {
+      std::swap(a[n], a[(n - 1) / 2]);
+      n = (n - 1) / 2;
+    }
+  }
 
-     int GetMin() {
-       return a[0];
-     }
+  void SiftDown(int n) {
+    while (2 * n + 1 <= size) {
+      if (2 * n + 2 <= n && a[2 * n + 2] < a[2 * n + 1] && a[n] > a[2 * n + 2]) {
+        std::swap(a[n], a[2 * n + 2]);
+        n = 2 * n + 2;
+      } else if (a[n] > a[2 * n + 1]) {
+        std::swap(a[n], a[2 * n + 1]);
+        n = 2 * n + 1;
+      } else {
+        break;
+      }
+    }
+  }
 
-     int ExtractMin() {
-       int min_val = a[0];
-       std::swap(a[0], a[size--]);
-       SiftDown(0);
-       return min_val;
-     }
+  int GetMin() {
+    return a[0];
+  }
 
-     void DecreaseKey(int i, int delta) {}
+  int ExtractMin() {
+    int min_val = a[0];
+    std::swap(a[0], a[size--]);
+    SiftDown(0);
+    return min_val;
+  }
+
+  void DecreaseKey(int i, int delta) {}
 };
